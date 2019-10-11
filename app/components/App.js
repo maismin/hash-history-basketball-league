af1167dcb812a35296bec65547598f0cca0ad541
Simon Mai <maismin@gmail.com>
2019-10-11 16:04:16 -0400

    Create navigation bar

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import NavBar from './NavBar';
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/players" component={Players} />
+        <Route path="/teams" component={Teams} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
