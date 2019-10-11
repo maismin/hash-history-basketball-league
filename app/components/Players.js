a7eca12c6da27ec4bcc09d78ef17f1c4a499483d
Simon Mai <maismin@gmail.com>
2019-10-11 19:20:49 -0400

    Create SideBar component

--- app/components/Players.js
+++ app/components/Players.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { parse } from 'query-string';
+import SideBar from './SideBar';
+import { getPlayers } from '../utils/api';
 
 function Players() {
-  return <div className="container two-column">Players</div>;
+  const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const location = useLocation();
+
+  const fetchPlayers = teamId => {
+    getPlayers(teamId).then(playersArr => {
+      setPlayers(playersArr);
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    if (location.search) {
+      fetchPlayers(parse(location.search).teamId);
+    } else {
+      fetchPlayers();
+    }
+  }, []);
+
+  return (
+    <div className="container two-column">
+      <SideBar
+        title="Players"
+        loading={loading}
+        list={players.map(player => player.name)}
+      />
+      {!loading && location.pathname === '/players' ? (
+        <div className="sidebar-instruction">Select a Player</div>
+      ) : null}
+    </div>
+  );
 }
 
 export default Players;
---
+++ app/components/SideBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink, useRouteMatch } from 'react-router-dom';
+import slug from 'slug';
+
+function SideBar({ title, list, loading }) {
+  const match = useRouteMatch();
+
+  return loading ? (
+    <h1>LOADING</h1>
+  ) : (
+    <div>
+      <h3 className="header">{title}</h3>
+      <ul className="sidebar-list">
+        {list.map(item => (
+          <NavLink
+            key={item}
+            to={`${match.url}/${slug(item)}`}
+            activeStyle={{ listStyleType: 'none', fontWeight: 'bold' }}
+          >
+            {item.toUpperCase()}
+          </NavLink>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+SideBar.propTypes = {
+  title: PropTypes.string.isRequired,
+  list: PropTypes.arrayOf(PropTypes.string).isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default SideBar;
