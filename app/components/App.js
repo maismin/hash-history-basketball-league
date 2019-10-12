60d82eb5afa7028ba85a3db77ba3cca8afca6179
Simon Mai <maismin@gmail.com>
2019-10-12 13:38:40 -0400

    Add dynamic imports to project

--- app/components/App.js
+++ app/components/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Loading from './Loading';
 import NavBar from './NavBar';
-import Home from './Home';
-import Players from './Players';
-import Teams from './Teams';
-import TeamPage from './TeamPage';
-import Articles from './Articles';
+
+const Home = React.lazy(() => import('./Home'));
+const Players = React.lazy(() => import('./Players'));
+const Teams = React.lazy(() => import('./Teams'));
+const TeamPage = React.lazy(() => import('./TeamPage'));
+const Articles = React.lazy(() => import('./Articles'));
 
 function App() {
   return (
     <Router>
       <div>
         <NavBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/players" component={Players} />
-          <Route path="/teams" component={Teams} />
-          <Route path="/:teamId" exact component={TeamPage} />
-          <Route path="/:teamId/articles" component={Articles} />
-          <Route
-            render={() => <h1 className="text-center">Four Oh Four.</h1>}
-          />
-        </Switch>
+        <React.Suspense fallback={<Loading />}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/players" component={Players} />
+            <Route path="/teams" component={Teams} />
+            <Route path="/:teamId" exact component={TeamPage} />
+            <Route path="/:teamId/articles" component={Articles} />
+            <Route
+              render={() => <h1 className="text-center">Four Oh Four.</h1>}
+            />
+          </Switch>
+        </React.Suspense>
       </div>
     </Router>
   );
